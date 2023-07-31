7877859e419178d32b7d7657db8be19a5edca624
Karassay Raushanbek <karassayraushanbek@gmail.com>
2023-07-31 21:56:42 +0600

    Init

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,23 @@
+<template>
+  <q-layout view="hhh lpR fFf">
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+
+    <q-footer class="bg-custom text-black">
+      <q-toolbar>
+        <q-toolbar-title>
+          <div class="flex flex-center">TEST</div>
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-footer>
+  </q-layout>
+</template>
+
+<script setup lang="ts"></script>
+
+<style>
+.bg-custom {
+  background-color: rgba(16, 18, 28, 0.03);
+}
+</style>
